cef72f538bd282b79287e4900771faa8b2635063
Ali Anwar <greenywish@gmail.com>
2020-10-02 01:11:46 +0530

    Update password.py

--- knot65/password.py
+++ knot65/password.py
@@ -6,9 +6,3 @@ for char in f:
 		password+=char
 print(password) #IKNOWYARECOLLECTAINGLLUPPERCASELETTERFIRSTPARTOFTHEPASSWORDISLETTHEREBE
 print("The first part of password is",password[-10:]) #LETTHEREBE
-
-thirdPart=""
-for char in f:
-        if ord(char)>65310:
-                thirdPart+=char
-print("Third part of the password "+thirdPart)
